a244d813f216ad64c155f5ae586702fd394c91fe
MarianaLimon <marianalimong@hotmail.com>
2021-03-18 22:04:42 -0600

    ADD sesion 9 JavaScript

---
+++ s9JavaScript/js/main.js
@@ -0,0 +1,63 @@
+//window.alert("hola koders");
+
+/*
+console.log("Hola Koders!!!");
+
+var variable1 = "Isrrael Salinas"
+
+console.log(typeof variable1)
+
+var variable2 = 27
+
+console.log(typeof variable2)
+
+var variable3 = {name:"Mariana Limon"}
+
+var variable4 = false
+
+var variable5;
+
+var variable6 = null;
+
+var vaariable7 = ["a", 27, {name:"Mariana"}]
+
+console.log(typeof variable3)
+console.log(typeof variable4)
+console.log(typeof variable5)
+
+console.log(variable5)
+
+console.log(typeof variable6)
+console.log(variable6)
+
+console.log(typeof variable7)
+*/
+
+/* Crear 2 variables 
+cada variable almacena un numero
+imprimir en consola el resultado de cada una de las 
+operaciones aritmeticas entre los numeros */
+
+var num1 = 10
+var num2 = 3
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+console.log(++num1);
+console.log(--num2);
+
+document.writeln("<h1>Hola</h1>");
+document.writeln("<p>Koders</p>");
+
+document.writeln("<h1>" + (num1 + num2) + "</h1>");
+
+var additioResult = num1 + num2
+document.writeln("<h1>" + additioResult + "</h1>");
+
+var name = "Mariana"
+var lastName = "Limon Granados"
+
+document.writeln("<h2>" + name + " " + lastName + "</h2>");
